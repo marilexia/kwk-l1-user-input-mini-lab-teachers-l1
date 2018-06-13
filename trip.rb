6d9b36cb7f9a567ac705c4f94e6a302238f4afb0
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-13 21:59:43 +0000

    automatically backed up by learn

--- trip.rb
+++ trip.rb
@@ -1,5 +1,5 @@
 # Code your prompts here!
-
+puts "Hello, traveler!"
 # First, puts out a string asking where the tourist would like to stay.
 puts "Let's plan your trip! Where would you like to stay?"
 
